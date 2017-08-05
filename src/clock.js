16cc0971f3464ec6cd69a3f3a7978320322802f3
Cem <cemozturan@gmail.com>
2017-08-05 16:10:17 +0100

    Adding leading 0s to deadline fields

--- src/clock.js
+++ src/clock.js
@@ -20,12 +20,16 @@ class Clock extends Component {
 		setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
 	}
 
+	leading0(num) {
+		return (num < 10) ? `0${num}` : num;
+	}
+
 	getTimeUntil(deadline) {
 		const time = Date.parse(deadline) - Date.parse(new Date());
-		const seconds = Math.floor((time/1000)%60);
-		const minutes = Math.floor((time/1000/60)%60);
-		const hours = Math.floor(time/(1000*60*60)%24);
-		const days = Math.floor(time/(1000*60*60*24));
+		const seconds = this.leading0(Math.floor((time/1000)%60));
+		const minutes = this.leading0(Math.floor((time/1000/60)%60));
+		const hours = this.leading0(Math.floor(time/(1000*60*60)%24));
+		const days = this.leading0(Math.floor(time/(1000*60*60*24)));
 		this.setState(() => {
 			return {
 				days,
