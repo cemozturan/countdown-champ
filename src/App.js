7cc8c4a83486fac7ed84b466431e1ad693d81cd5
Cem <cemozturan@gmail.com>
2017-08-05 15:10:46 +0100

    adding state with deadline

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React, {Component} from 'react';
 import './app.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-		  <div>
-			  <div className="app-title">Countdown to December 25, 2017</div>
-			  <div className="clock-days">14 days</div>
-			  <div className="clock-hours">12 hours</div>
-			  <div className="clock-minutes">15 minutes</div>
-			  <div className="clock-seconds">23 seconds</div>
-		  </div>
-		  <div>
-			  <input placeholder="new date here" />
-			  <button>Submit</button>
-		  </div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			deadline: 'December 25, 2017'
+		};
+		this.changeDeadline = this.changeDeadline.bind(this);
+	}
+
+	changeDeadline() {
+		this.setState(() => {
+			return {
+				deadline: 'October 15, 1987'
+			};
+		});
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<div>
+					<div className="app-title">Countdown to {this.state.deadline}</div>
+					<div className="clock-days">14 days</div>
+					<div className="clock-hours">12 hours</div>
+					<div className="clock-minutes">15 minutes</div>
+					<div className="clock-seconds">23 seconds</div>
+				</div>
+				<div>
+					<input placeholder="new date here" />
+					<button onClick={this.changeDeadline}>Submit</button>
+				</div>
+			</div>
+    	);
+	}
 }
 
 export default App;
