c25f7704ced6a54f990f789163e4d5d3e6d086d7
Cem <cemozturan@gmail.com>
2017-08-05 15:02:11 +0100

    adding hard-coded time and input element + some basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello world
+      <div className="app">
+		  <div>
+			  <div className="app-title">Countdown to December 25, 2017</div>
+			  <div className="clock-days">14 days</div>
+			  <div className="clock-hours">12 hours</div>
+			  <div className="clock-minutes">15 minutes</div>
+			  <div className="clock-seconds">23 seconds</div>
+		  </div>
+		  <div>
+			  <input placeholder="new date here" />
+			  <button>Submit</button>
+		  </div>
       </div>
     );
   }
